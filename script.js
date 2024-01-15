c0effeca587574e7e3ff529f590a69747536c6c7
Atendimento2005 <gogo.june2005@gmail.com>
2024-01-15 18:08:04 +0530

    completed frontend

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener("load", () => {
+  const submitButton = document.querySelector('#submit-button')
+  
+  submitButton.addEventListener("click", () => {
+    // open("/comingsoon.html")
+    window.location.assign("/comingsoon.html")
+  })
+})
