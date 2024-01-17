55d3ef99037fcfa640abf4c048595ba4639533bb
Atendimento2005 <gogo.june2005@gmail.com>
2024-01-17 21:53:28 +0530

    implemented js functionality

--- script.js
+++ script.js
@@ -6,9 +6,7 @@ window.addEventListener("load", () => {
   const weightUnit = document.querySelector('#weight-unit')
   const formula = document.querySelector('#formula');
   const bodyFat = document.querySelector('#body-fat');
-  const age = document.querySelector('#age');
-  const bmrDisplay = document.querySelector('#bmr-display');
-  
+  const age = document.querySelector('#age');  
   
   formula.addEventListener('change', () => {
     if (formula.value == 'katch-mcardle'){
@@ -45,8 +43,6 @@ window.addEventListener("load", () => {
       validCount+=1
     }
 
-    console.log(height.value)
-
     if(!weight.value){
       weight.setCustomValidity("Please enter your weight")
     } else {
@@ -68,21 +64,59 @@ window.addEventListener("load", () => {
       validCount += 1
     }
 
-    if (validCount == 4) {
+    if(document.querySelector('input[name="gender"]:checked').value){
+      validCount += 1
+    }
+
+    if (validCount == 5) {
      
+      let genderValue = document.querySelector('input[name="gender"]:checked').value
+      let ageValue = Number(age.value)
+      let formulaValue = formula.value
+      let heightValue = (heightUnit.value == 'in') ? Math.round(height.value*2.54) : Number(height.value)
+      let weightValue = (weightUnit.value == 'lb') ? Math.round(weight.value/2.205) : Number(weight.value)  
+      let bodyFatValue = bodyFat.value/100    
+
       let bmr;
 
-      // TODO Get value of gender and complete this
-      if(formula.value = 'mifflin-st') {
-        bmr = 10*weight.value + 6.25*height.value - 5*age.value + 5
-        
-      } else if (formula.value = 'harris-benedict') {
-        bmr = 1000
+
+      if(formulaValue == 'mifflin-st') {
+        if(genderValue == 'male'){
+          bmr = 10*weightValue + 6.25*heightValue - 5*ageValue + 5
+        } else if (genderValue == 'female'){
+          bmr = 10*weightValue + 6.25*heightValue - 5*ageValue - 161
+        }
+      } else if (formulaValue == 'harris-benedict') {
+        if(genderValue == 'male'){
+          bmr = 13.397*weightValue + 4.799*heightValue - 5.677*ageValue + 88.362
+        } else if (genderValue = 'female'){
+          bmr = 9.247*weightValue + 3.098*heightValue - 4.330*ageValue + 447.593
+        }
+      } else if(formulaValue == 'katch-mcardle') {
+        bmr = 370 + 21.6*(1 - bodyFatValue)*weightValue
       }
 
-      bmrDisplay.innerText = bmr
+      const bmrDisplay = document.querySelector('#bmr-display');
+      const calorieDisplays = document.querySelectorAll('.calorie-display')
+      
+      bmrDisplay.innerText = Math.round(bmr)
+      
+      calorieDisplays[0].innerText = Math.round(bmr*1.2)
+      calorieDisplays[1].innerText = Math.round(bmr*1.375)
+      calorieDisplays[2].innerText = Math.round(bmr*1.465)
+      calorieDisplays[3].innerText = Math.round(bmr*1.55)
+      calorieDisplays[4].innerText = Math.round(bmr*1.725)
+      calorieDisplays[5].innerText = Math.round(bmr*1.9)
 
       document.querySelector("#results").classList.remove('hidden')
+      
+      window.scrollTo({
+        top: document.querySelector("#results").getBoundingClientRect().top + document.documentElement.scrollTop,
+        left: 0,
+        behavior: 'smooth',
+      })
+      
+      document.querySelector("#results")
     }
 
   })
